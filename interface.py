50043bf1ad5ce8092ffee31321b9dcc60f5c818e
fotinosk <fotinos.kyriakides@gmail.com>
2020-10-15 23:33:03 +0100

    Added function to get model state and output. (Must add functionality to plot output against time)

--- interface.py
+++ interface.py
@@ -1,7 +1,8 @@
 """Interface between controller (slider) and model"""
 
-from slider_test import user_input
+from user_input import user_input
 from multiprocessing import Process, Pipe
+from state_space_model import model
 import sys
 
 
@@ -9,10 +10,20 @@ if __name__ == '__main__':
     parent_conn, child_conn = Pipe()
     p = Process(target=user_input, args=(child_conn,))
     p.start()
+    input_time = [[],[]]
     while True:
+        # change format to bunches of 20 and to input array, time array
         try:
-            print(parent_conn.recv())
+            received = parent_conn.recv()
+            print(received)
+            input_time[0].append(received[0]), input_time[1].append(received[1])
             child_conn.close()
+            state = None
+            if len(input_time[0]) == 20:
+                print(input_time)
+                yout, state, time = model(input_time[1], input_time[0], state)
+                print(yout,state,time)
+                input_time = [[], []]
         except:
             print('Shutting down')
             sys.exit()
---
+++ state_space_model.py
@@ -0,0 +1,38 @@
+"""
+Module for user controlled input to the model.
+User constantly inputs data using slider and output is plotted on graph.
+"""
+
+import control
+from scipy import signal
+
+A = [[0.00187, 0.0263, -86.15, -31.939],
+     [-0.07, -0.2941, 672.9, -4.12],
+     [0.002653, -0.0009834, -0.3449, 0.0003729],
+     [0, 0, 1, 0]]
+B = [[1.93],
+     [-15.21],
+     [-0.9686],
+     [0]]
+C = [[0, 0, 0, 1]]
+D = [0]
+
+sign = signal.lti(A, B, C, D)
+
+
+# print(sign.output([10, 10, 10, 30, 300, 10, 20, 50], [1, 2, 3, 4, 5, 6, 7, 8]))
+# inputs and timestamps and outputs the output and state
+
+
+def model(time, inputs, state):
+    """
+     :param time: Array containing the time intervals
+     :param inputs: Array containing the inputs
+     :param state: Array containing the state (initially None?)
+     :return: outputs and last state, plot(?)
+     """
+    t, yout, xout = sign.output(inputs, time, X0=state)
+
+    # TODO: add code so that the output can be plotted
+
+    return yout, xout[-1], time
