05bc169a5b56ae980f7c497b58e81f40cef714d6
fotinosk <fotinos.kyriakides@gmail.com>
2020-10-15 20:20:44 +0100

    Added slider module for user controller

---
+++ interface.py
@@ -0,0 +1,6 @@
+"""Interface between controller (slider) and model"""
+
+from slider_test import user_input
+
+while True:
+    print(user_input())
---
+++ slider_test.py
@@ -0,0 +1,48 @@
+"""
+Slider that acts as a controller for the plane and streams its values.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import sys
+
+time = 0.00
+
+
+def user_input():
+    a_min = -100  # the minimial value of the paramater a
+    a_max = 100  # the maximal value of the paramater a
+    a_init = 0  # the value of the parameter a to be used initially, when the graph is created
+
+    dt = 0.05
+
+    def handle_close(evt):
+        sys.exit()
+
+    def onclick(event):
+        global time
+
+        # yield a_slider.val  # instead of print, yield (?)
+        print(time, a_slider.val)
+
+        plt.pause(dt)
+        time += dt
+        time = round(time, 2)
+        onclick('1')
+
+    fig = plt.figure(figsize=(8, 3))
+    fig.canvas.mpl_connect('close_event', handle_close)
+    fig.canvas.mpl_connect('button_press_event', onclick)
+
+    slider_ax = plt.axes([0.1, 0.25, 0.8, 0.5])
+
+    a_slider = Slider(slider_ax, 'Slider', a_min, a_max, valinit=a_init)
+
+    plt.plot()
+    plt.show()
+
+
+# for (t,x) in user_input():
+#     print(t,x)
+user_input()
