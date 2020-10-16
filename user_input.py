ecb1d5f47a4bb02cf3a7d1273cd24c1dfe816f49
fotinosk <fotinos.kyriakides@gmail.com>
2020-10-16 23:46:36 +0100

    Added second slider to control thrust. Thrust value is sent, but not supported by the model

--- user_input.py
+++ user_input.py
@@ -15,6 +15,10 @@ def user_input(conn):
     a_max = 100  # the maximal value of the paramater a
     a_init = 0  # the value of the parameter a to be used initially, when the graph is created
 
+    b_min = - 50
+    b_max = 200
+    b_init = 0
+
     dt = 0.05
 
     def handle_close(evt):
@@ -26,19 +30,21 @@ def user_input(conn):
 
         fig.canvas.mpl_disconnect(click)  # disconnects so that second click doesnt raise errors
 
-        conn.send([a_slider.val, time])
+        conn.send([a_slider.val, time, b_slider.val]) # dont change order, user in inteface on this order
         plt.pause(dt)
         time += dt
         time = round(time, 2)
         onclick('1')  # placeholder
 
-    fig = plt.figure(figsize=(8, 3))
+    fig = plt.figure(figsize=(8, 6))
     fig.canvas.mpl_connect('close_event', handle_close)
     click = fig.canvas.mpl_connect('button_press_event', onclick)
 
-    slider_ax = plt.axes([0.1, 0.25, 0.8, 0.5])
+    slider_ax1 = plt.axes([0.15, 0.5, 0.75, 0.3])
+    slider_ax2 = plt.axes([0.15, 0.1, 0.75, 0.3])
 
-    a_slider = Slider(slider_ax, 'Slider', a_min, a_max, valinit=a_init)
+    a_slider = Slider(slider_ax1, 'Elevator Angle', a_min, a_max, valinit=a_init)
+    b_slider = Slider(slider_ax2, 'Thrust', b_min, b_max, valinit=b_init)
 
     plt.plot()
     plt.show()
