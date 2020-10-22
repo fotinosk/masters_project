2549914ce6501adb4864a924fd9b6acba0b763ba
fotinosk <fotinos.kyriakides@gmail.com>
2020-10-22 20:20:40 +0100

    Changes initial states to be closer to equilibrium

--- initial_state_generator.py
+++ initial_state_generator.py
@@ -12,19 +12,26 @@ def state(danger=False):
     If not in danger mode, then at least one of the states must be to the extremes.
     """
 
-    # TODO: Change the states to be close to system equilibrium
+    """
+    for x_dot = 0:   ie no change to the state
+        1. q = 0
+        2. u = 0
+        3. v = htta
+        4. T = 3 * theta
+        
+    reasonable starting point: u = [1,3] x = [1,0,0,1]
+    """
 
     if not danger:
-        f_vel = random.randint(260, 320)  # typical cruising speed of 293m/s
-        v_vel = random.randint(-12, 12)  # typical descent speed of 9m/s
-        p_rate = 10 * random.random() - 5  # guessed pitch rates between [-5, 5]
-        p_angle = random.randint(-10, 10)  # typical pitch angle of 0 deg
+        f_vel = 1 + (random.random()-0.5) * 0.2     # 0.9 - 1.1
+        v_vel = (random.random()-0.5) * 0.4         # -0.2 - 0.2
+        p_rate = (random.random()-0.5) * 0.2        # -0.1 - 0.1
+        p_angle = 1 + (random.random()-0.5) * 0.3   # 0.7 - 1.3
 
     else:
-        f_vel = random.randint(50, 350)
-        v_vel = random.randint(-50, 20)
-        p_rate = 15 * random.random() - 10  # [-10, 5]
-        p_rate = round(p_rate, 3)
-        p_angle = random.randint(-20, 20)
+        f_vel = 1 + (random.random()-0.5) * 0.4     # 0.6 - 1.4
+        v_vel = (random.random()-0.5)               # -0.5 - 0.5
+        p_rate = (random.random()-0.5) * 0.3        # -0.2 - 0.2
+        p_angle = 1 + (random.random()-0.5) * 0.5   # 0.5 - 1.5
 
     return [f_vel, v_vel, p_rate, p_angle]
