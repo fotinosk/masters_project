5d784791034f78773a5ff6bb9f6cb432d5926a81
fotinosk <fotinos.kyriakides@gmail.com>
2021-02-27 01:00:20 +0200

    Added batch functionality to division

--- Division/agent.py
+++ Division/agent.py
@@ -3,6 +3,9 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import sys
+
+batch_size = 100
 
 class Divisor(nn.Module):
     def __init__(self):
@@ -12,8 +15,11 @@ class Divisor(nn.Module):
 
     def forward(self, x):
         x = self.linear1(x)
+        # print('l1 ', x)
         x = torch.tanh(x)
+        # print('tanh ', x)
         x = self.linear2(x)
+        # print('l2 ', x)
         return x
 
 div = Divisor()
@@ -24,12 +30,15 @@ optimizer = optim.Adam(div.parameters(), lr=0.0001)
 # training loop
 running_loss = 0.0
 
-for step in range(10**6):
+for step in range(10**8):
 
     optimizer.zero_grad()
-    x = (torch.rand(2) - 0.5) * 200  # range [-100,100]
-    ratio = x[0] / x[1]
-    ratio = ratio.unsqueeze(0)
+    x = (torch.rand(batch_size, 2) - 0.5) * 200  # range [-100,100]
+    ratio = x[:, 0] / x[:, 1]
+    for i in range(len(x)):
+        if abs(x[i,1]) < 0.000001:
+            x = torch.cat([x[:i], x[i+1:]])
+    ratio = ratio.unsqueeze(1)
     z = div(x)
     loss = criterion(z, ratio)
     loss.backward()
@@ -46,11 +55,6 @@ for step in range(10**6):
             y = (torch.rand(2) - 0.5) * 200
             r = y[0] / y[1]
             q = div(y)
-            print(f"Ratio: {y[0].item():.2f}/{y[1].item():.2f} | Right Answer: {r.item():.2f} | Net Output: {q.item():.2f}")
+            print(f"Right Answer: {r.item():.2f} | Net Output: {q.item():.2f}")
+        print("\n\n")
 
-"""
-TODO:
-1. Surround training loop so that it happens multiple times
-2. Display results (split into epochs and show results after each)
-3. Add functionality to save model
-"""
